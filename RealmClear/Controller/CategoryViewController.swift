0643db44461cd4fb78ea20a6e4b93cc3796fac3a
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-08-01 15:19:20 -0500

    Setup CategoryViewController UI

---
+++ RealmClear/Controller/CategoryViewController.swift
@@ -0,0 +1,108 @@
+//
+//  CategoryViewController.swift
+//  RealmClear
+//
+//  Created by Joe Vargas on 8/1/22.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Categories"
+        
+        loadCategories()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupNavBarUI()
+        setupTableViewUI()
+    }
+    
+    // MARK: - UI Setup
+    
+    private func setupNavBarUI(){
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    private func setupTableViewUI(){
+        tableView.separatorStyle = .none
+    }
+    
+    // MARK: - Selector Functions
+    
+    @objc private func addButtonPressed(){
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { action in
+            
+//            let newCategory = Category(context: self.context)
+//            newCategory.name = textfield.text!
+//
+//            self.categories.append(newCategory)
+//            self.saveCategories()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Category name"
+            textField = alertTextField
+        }
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Helper Functions
+    
+    private func saveCategories() {
+//        do {
+//            try context.save()
+//        } catch {
+//            print("DEBUG: Error saving context \(error)")
+//        }
+        
+        self.tableView.reloadData()
+    }
+    
+    private func loadCategories(){
+//        let request : NSFetchRequest<Category> = Category.fetchRequest()
+//        do {
+//            categories = try context.fetch(request)
+//        } catch {
+//            print("DEBUG: Error fetching data from context \(error)")
+//        }
+        
+        tableView.reloadData()
+    }
+}
+
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+//        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destinationVC = ToDoListViewController()
+//        destinationVC.selectedCategory = categories[indexPath.row]
+        self.navigationController?.pushViewController(destinationVC, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ RealmClear/Controller/ToDoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoListViewController.swift
+//  RealmClear
+//
+//  Created by Joe Vargas on 8/1/22.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
