6f3aad40463a8dbdc8a7307c02c048aab16aa95f
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-08-01 14:35:06 -0500

    Add skeleton project

---
+++ RealmClear/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmClear
+//
+//  Created by Joe Vargas on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
