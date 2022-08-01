d4a34dc6582fccfebf459d41257e9d8b8a3bbb21
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-08-01 14:50:33 -0500

    Setup Nav Controller and added CategoryViewController to the stack

--- RealmClear/Controller/ViewController.swift
+++ RealmClear/Controller/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Categories"
     }
 
 
